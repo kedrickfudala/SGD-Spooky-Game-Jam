e03709adebf0dadae71ae210b25f7023a1ceda93
kedrickfudala <KedrickAFudala@gmail.com>
2024-10-18 19:04:44 -0400

    Level setup

--- src/levels/world.gd
+++ src/levels/world.gd
@@ -4,11 +4,22 @@ class_name World
 @onready var player : PackedScene = preload("res://src/player/player.tscn")
 @onready var player_inst : Object = null
 
+@onready var level_0 : PackedScene = preload("res://src/levels/level_0.tscn")
+
+@onready var levels = [level_0]
+
 
 func _ready():
+	spawn_player()
+	spawn_level(0)
 	pass
 
 func spawn_player():
 	player_inst = player.instantiate()
 	player_inst.global_position = Vector2(0,0)
 	add_child(player_inst)
+
+func spawn_level(id : int):
+	var level_inst = levels[id].instantiate()
+	level_inst.global_position = Vector2(0,0)
+	add_child(level_inst)
--- src/player/player.gd
+++ src/player/player.gd
@@ -4,9 +4,23 @@ class_name Player
 @onready var score : int = 0
 @onready var combo : float = 0.0
 
+@onready var jumps : int = 2
+
 func _ready():
 	pass
 	
 func _physics_process(delta: float) -> void:
-	velocity.y += 400
-	
+	handle_input()
+	handle_movement()
+
+func handle_input():
+	if is_on_floor():
+		jumps = 2
+	if Input.is_action_just_pressed("jump") and jumps > 0:
+		velocity.y -= 300
+		jumps -= 1
+
+func handle_movement():
+	velocity.x += 5
+	velocity.y += 9.8
+	move_and_slide()
